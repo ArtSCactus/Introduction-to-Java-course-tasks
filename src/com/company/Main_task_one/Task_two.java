f9cf266375c6e4ea9cf4a1e5e865226bbb528582
Artsiom (ArtSCactus) <artemir14@gmail.com>
2019-09-16 12:35:24 +0300

    Update Task_two javadoc

--- src/com/company/Main_task_one/Task_two.java
+++ src/com/company/Main_task_one/Task_two.java
@@ -10,8 +10,8 @@ public class Task_two {
 
     /**Converts mass, that given in kilograms into the milligrams.
      *
-     * @param mass
-     * @return
+     * @param mass in kilograms
+     * @return mass in milligrams
      */
     private static double toMilligrams (double mass){
         return mass*1000000;
@@ -19,8 +19,8 @@ public class Task_two {
 
     /**Converts mass, that given in kilograms into the grams.
      *
-     * @param mass
-     * @return
+     * @param mass in kilograms
+     * @return mass in grams
      */
     private static double toGrams (double mass){
         return mass*1000;
@@ -28,8 +28,8 @@ public class Task_two {
 
     /**Converts mass, that given in kilograms into the tons.
      *
-     * @param mass
-     * @return
+     * @param mass in kilograms
+     * @return mass in tons
      */
     private static double toTons(double mass){
         return mass/1000;
@@ -47,6 +47,7 @@ public class Task_two {
             System.out.print("Invalid data. Dinosaur mass cannot be less than 0. Try again: ");
             massInKG = input.nextDouble();
         }
-        System.out.println("Answer:\nIn tons "+toTons(massInKG)+"\nIn grams: "+toGrams(massInKG)+"\nIn milligrams: "+toMilligrams(massInKG));
+        System.out.println("Answer:\nIn tons "+toTons(massInKG)+"\nIn grams: "+toGrams(massInKG)+
+                "\nIn milligrams: "+toMilligrams(massInKG));
     }
 }
