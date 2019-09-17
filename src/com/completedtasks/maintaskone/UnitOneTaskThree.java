5a141159a1e17ca67fd30edcddd7199f9f608411
Artsiom (ArtSCactus) <artemir14@gmail.com>
2019-09-17 11:50:36 +0300

    Fixed task №3

--- src/com/completedtasks/maintaskone/UnitOneTaskThree.java
+++ src/com/completedtasks/maintaskone/UnitOneTaskThree.java
@@ -17,7 +17,7 @@ public class UnitOneTaskThree {
      */
     private static double findSquare(double interiorRadius, double externalRadius){
         if (interiorRadius==externalRadius) return 0;
-        else return Math.PI*Math.pow(interiorRadius,2)-Math.PI*Math.pow(externalRadius,2);
+        else return Math.PI*Math.pow(externalRadius,2)-Math.PI*Math.pow(interiorRadius,2);
     }
 
     /**Launches application. Contains console interface.
@@ -25,17 +25,17 @@ public class UnitOneTaskThree {
      */
     public static void launch_app() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Please, input 3 integer numbers");
-        double interiorRadius=input.nextDouble();
+        System.out.println("Please, interior radius and external radius:\nExternal radius: ");
         double externalRadius=input.nextDouble();
-
-        //User input validation. Innterior radius must be less or equals than external.
+        System.out.println("Interior radius: ");
+        double interiorRadius=input.nextDouble();
+        //User input validation. Interior radius must be less or equals than external.
         while (interiorRadius > externalRadius) {
             interiorRadius = input.nextDouble();
             externalRadius = input.nextDouble();
             if (interiorRadius>externalRadius) System.out.println("Invalid data. Interior radius MUST be less or equals external.");
         }
 
-        System.out.println("Answer: The ring square is " + UnitOneTaskThree.findSquare(interiorRadius, externalRadius));
+        System.out.println("The ring square is " + UnitOneTaskThree.findSquare(interiorRadius, externalRadius));
     }
 }
